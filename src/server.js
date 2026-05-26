83de837a3b4a95da65268f8c538ef20471900269
manitthongkaw <manitthongkaw@gmail.com>
2026-05-26 13:36:12 +0700

    first setup crud routes for products

--- src/server.js
+++ src/server.js
@@ -7,6 +7,22 @@ app.get('/', (req, res) => {
   res.send('Welcome to JSD12 (Backend Assessment) - 14_Manit (Arm/อาม) (O_o)i')
 })
 
+app.get("/products", (req, res) => {
+
+});
+app.get("/products/:id", (req, res) => {
+
+});
+app.post("/products", (req, res) => {
+
+});
+app.put("/products/:id", (req, res) => {
+
+});
+app.delete("/products/:id", (req, res) => {
+
+});
+
 const port = 1414;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
